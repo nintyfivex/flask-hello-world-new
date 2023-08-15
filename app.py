799e72a40a3af3aa923985464133f9823810f39f
nintyfivex <141329871+nintyfivex@users.noreply.github.com>
2023-08-15 18:12:15 +0530

    Update app.py download

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_file
 from PIL import Image
 import random
 import os
+import zipfile
 
 app = Flask(__name__)
 
@@ -17,10 +18,13 @@ def process():
 
     image_files = request.files.getlist("image")
     watermark_files = request.files.getlist("watermarks")
-
+    download_format = request.form['download_format']  # New field for download format
+    
     # Create output folder if it doesn't exist
     output_folder = "output_folder"
     os.makedirs(output_folder, exist_ok=True)
+
+    processed_images = []
 
     while image_files:
         selected_image = image_files.pop()
@@ -37,11 +41,22 @@ def process():
 
         output_path = os.path.join(output_folder, f"watermarked_{selected_image.filename}")
         watermarked_img.save(output_path)
+        processed_images.append(output_path)
 
-    return "Watermarking complete."
+    # Create a zip file containing the processed images
+    zip_filename = 'processed_images.zip'
+    with zipfile.ZipFile(zip_filename, 'w') as zipf:
+        for image_path in processed_images:
+            zipf.write(image_path, os.path.basename(image_path))
+            os.remove(image_path)  # Remove the individual processed images
+
+    # Remove the output folder
+    os.rmdir(output_folder)
+
+    # Send the zip file for download
+    if download_format == 'zip':
+        return send_file(zip_filename, as_attachment=True)
 
 if __name__ == '__main__':
-    # Get the port from the environment variable, or use 8080 as a default
     port = int(os.environ.get('PORT', 8080))
-    # Start the app with binding to 0.0.0.0 to allow external connections
     app.run(host='0.0.0.0', port=port)
